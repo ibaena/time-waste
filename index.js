d1a8445c5bb7b0f15741051069eedb8ea1065ea9
Ivan <ivan.baena04@gmail.com>
2016-02-24 00:55:04 -0500

    initialized

---
+++ index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.listen('3000', function(){
+console.log('This is working!');
+});
